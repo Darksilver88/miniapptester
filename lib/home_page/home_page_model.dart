07981795f13f412181cd31e8f5307eb06b9fe179
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-12 04:57:07 +0000

    Updating to latest FlutterFlow output.

--- lib/home_page/home_page_model.dart
+++ lib/home_page/home_page_model.dart
@@ -8,6 +8,7 @@ import '/flutter_flow/custom_functions.dart' as functions;
 import 'dart:async';
 import 'home_page_widget.dart' show HomePageWidget;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:webviewx_plus/webviewx_plus.dart';
