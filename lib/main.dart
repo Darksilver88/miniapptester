c1d4a2dab37c81fd6d8063fc66851b2ded9847b2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-19 04:58:39 +0000

    Updating to latest FlutterFlow output.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'index.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  GoRouter.optionURLReflectsImperativeAPIs = true;
   usePathUrlStrategy();
 
   await FlutterFlowTheme.initialize();
